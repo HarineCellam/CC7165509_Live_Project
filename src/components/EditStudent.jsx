8b28d7c20ca3a5efed3f250016b170bd5e47ece9
Harine Cellam <harinecellam2210730@ssn.edu.in>
2025-06-16 23:19:46 +0530

    commit

--- src/components/EditStudent.jsx
+++ src/components/EditStudent.jsx
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import StudentForm from "./StudentForm";
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
-function EditStudent(){
+import StudentForm from '../components/StudentForm';
 
-    const {id} = useParams;
+function EditStudent() {
+    const { id } = useParams();
     const navigate = useNavigate();
     const [student, setStudent] = useState(null);
-    
+
     useEffect(() => {
         const stored = JSON.parse(localStorage.getItem('students')) || [];
         const found = stored.find(s => s.id === parseInt(id));
         if(!found) return navigate('/');
         setStudent(found);
-    },[id,navigate]);
+    }, [id, navigate]);
 
     const handleUpdate = (updatedStudent) => {
         const stored = JSON.parse(localStorage.getItem('students')) || [];
         const updatedList = stored.map(s => s.id === parseInt(id) ? updatedStudent : s);
-        localStorage.setItem('students',JSON.stringify(updatedList));
+        localStorage.setItem('students', JSON.stringify(updatedList));
         navigate('/');
     }
-
-    return(
+    return (
         <div>
             {student && <StudentForm onAdd={handleUpdate} initialData={student} />}
         </div>
     )
 }
+
 export default EditStudent;
--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,21 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-function studentForm({onAdd, initialData}){
-    const [form, setForm] = useState(
-        {
-            name: "",
-            roll: "",
-            department: "",
-            email: "",
-            section: "A",
-        }
-    )
+function StudentForm({ onAdd, initialData }) {
+    const [form, setForm] = useState({
+        name: "",
+        roll: "",
+        department: "",
+        email: "",
+        section: "A",
+    })
 
     useEffect(() => {
-        if(initialData) {
-            setForm(initialData);
-        }
-    },[initialData])
+      if (initialData) setForm(initialData);
+    }, [initialData]);
 
     const handleChange = (e) => {
         setForm({
@@ -26,8 +22,9 @@ function studentForm({onAdd, initialData}){
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!form.name || !form.roll) return alert("Name and Roll are Required!");
-        onAdd({...form, id:initialData?.id|| Date.now() });
+        if (!form.name || !form.roll)
+          return alert("Name and Roll are required");
+        onAdd({ ...form, id: initialData?.id || Date.now() });
         setForm({
             name: "",
             roll: "",
@@ -37,18 +34,51 @@ function studentForm({onAdd, initialData}){
         });
     }
 
-    return(
-        <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded-xl max-w-md mx-auto dark:bg-gray-700 dark:text-white">
-            <h2 className="text-xl font-semibold mb-4">{initialData ? "Edit Student" : "Add New Student"}</h2>
-            <input className="w-full p-2 border mb-3" type="text" name="name" placeholder="Name" value={form.name} onChange={handleChange} required />
-            <input className="w-full p-2 border mb-3" type="text" name="roll" placeholder="Roll Number" value={form.roll} onChange={handleChange} required />
-            <input className="w-full p-2 border mb-3" type="text" name="department" placeholder="Department" value={form.department} onChange={handleChange} />
-            <input className="w-full p-2 border mb-3" type="email" name="email" placeholder="Email" value={form.email} onChange={handleChange} />
-            <button type="submit" className="bg-blue-500 text-black py-2 px-4 rounded">
-                {initialData ? "Update Student" : "Add Student"}
-            </button>
-        </form>
-    )
-}
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-300 p-6 shadow-md rounded-xl max-w-md mx-auto"
+      >
+        <h2 className="text-xl font-semibold mb-4">{initialData ? "Edit Student" : "Add New Student"}</h2>
+        <input
+          className="w-full p-2 border mb-3"
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={form.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          className="w-full p-2 border mb-3"
+          type="text"
+          name="roll"
+          placeholder="Roll Number"
+          value={form.roll}
+          onChange={handleChange}
+          required
+        />
+        <input
+          className="w-full p-2 border mb-3"
+          type="text"
+          name="department"
+          placeholder="Department"
+          value={form.department}
+          onChange={handleChange}
+        />
+        <input
+          className="w-full p-2 border mb-3"
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={handleChange}
+        />
 
-export default studentForm;
+        <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">
+            { initialData ? "Update Student" : "Add Student" }
+        </button>
+      </form>
+    );
+}
+export default StudentForm;
